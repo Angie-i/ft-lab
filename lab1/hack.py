a1eca575be76688ed244db2a772475505d8c8a59
Angelina <st131131@apmath.info>
2025-09-25 12:31:37 +0300

    Update hack.py

--- lab1/hack.py
+++ lab1/hack.py
@@ -1,25 +1,58 @@
-from caesar import encrypt, decrypt
-from collections import Counter
-frequency_letter = {'e': 12.70, 't': 9.06, 'a': 8.17, 'o': 7.51, 'i': 6.97,
-        'n': 6.75, 's': 6.33, 'h': 6.09, 'r': 5.99, 'd': 4.25,
-        'l': 4.03, 'c': 2.78, 'u': 2.76, 'm': 2.41, 'w': 2.36,
-        'f': 2.23, 'g': 2.02, 'y': 1.97, 'p': 1.93, 'b': 1.49,
-        'v': 0.98, 'k': 0.77, 'x': 0.15, 'j': 0.15, 'q': 0.10, 'z': 0.07}
+from caesar import decrypt
+
+frequency_letter = {
+    'e': 12.70, 't': 9.06, 'a': 8.17, 'o': 7.51, 'i': 6.97,
+    'n': 6.75, 's': 6.33, 'h': 6.09, 'r': 5.99, 'd': 4.25,
+    'l': 4.03, 'c': 2.78, 'u': 2.76, 'm': 2.41, 'w': 2.36,
+    'f': 2.23, 'g': 2.02, 'y': 1.97, 'p': 1.93, 'b': 1.49,
+    'v': 0.98, 'k': 0.77, 'x': 0.15, 'j': 0.15, 'q': 0.10, 'z': 0.07
+}
+
 def hack(ciphertext: str) -> tuple[str, int]:
-        best_match=0
-        best_shift=0
-        best_text=''
-        l=len(ciphertext)
-        for x in range(26):
-                decrypted_text = decrypt(ciphertext, x)
-                frequency_decypt={}
-                for x, y in Counter(decrypted_text).items():
-                        if x.upper() in frequency_letter:
-                                frequency_decypt[x.upper()]= y/l*100
-                match_quality = sum(min(frequency_decypt.get(letter, 0), frequency_letter[letter])
-                                  for letter in frequency_letter)
-                if best_match< match_quality:
-                        best_match=match_quality
-                        best_shift=x
-                        best_text=decrypted_text
-        return best_text,best_shift
+    best_match = -1.0
+    best_shift = 0
+    best_text = ""
+
+    shift = 0
+    while shift < 26:
+        decrypted_text = decrypt(ciphertext, shift)
+
+        total = 0
+        counts = {}
+        i = 0
+        while i < len(decrypted_text):
+            ch = decrypted_text[i].lower()
+            if 'a' <= ch <= 'z':
+                total += 1
+                if ch in counts:
+                    counts[ch] += 1
+                else:
+                    counts[ch] = 1
+            i += 1
+
+       
+        freq_dec = {}
+        for letter in frequency_letter:
+            if total > 0 and letter in counts:
+                freq_dec[letter] = counts[letter] / total * 100
+            else:
+                freq_dec[letter] = 0.0
+
+       
+        match_quality = 0.0
+        for letter in frequency_letter:
+            a = freq_dec[letter]
+            b = frequency_letter[letter]
+            if a < b:
+                match_quality += a
+            else:
+                match_quality += b
+
+        if match_quality > best_match:
+            best_match = match_quality
+            best_shift = shift
+            best_text = decrypted_text
+
+        shift += 1
+
+    return best_text, best_shift
